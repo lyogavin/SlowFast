11bc649aa607cf51551d6d76708a3164211fd180
pi <l_y_o@pulsarsmarthome.cn>
2020-04-21 20:29:46 -0700

    test sh

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,2 @@
-python3 tools/run_net.py --cfg configs/Kinetics/SLOWFAST_8x8_R50.yaml DATA.PATH_TO_DATA_DIR /home/pi/ActivityNet/Crawler/Kinetics/k400/  TEST.CHECKPOINT_FILE_PATH ./SLOWFAST_8x8_R50.pkl  TEST.CHECKPOINT_TYPE caffe2 TRAIN.ENABLE False
+python3 tools/run_net.py --cfg configs/Kinetics/SLOWFAST_8x8_R50.yaml DATA.PATH_TO_DATA_DIR /home/pi/ActivityNet/Crawler/Kinetics/k400_2/  TEST.CHECKPOINT_FILE_PATH ./SLOWFAST_8x8_R50.pkl  TEST.CHECKPOINT_TYPE caffe2 TRAIN.ENABLE False
 
